9f9a84d8a4311b7aabe35042b59cf9702d954911
nacho131994 <nachommunoz13@gmail.com>
2023-02-24 13:09:24 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const icons = ["♦", "♥", "♠", "♣"];
+  const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  document.querySelector(".card").innerHTML = `
+  <div id="card">
+    <div id="top">${icons[0]}</div>
+    <div id="middle">${numbers[0]}</div>
+    <div id="bottom">${icons[0]}</div>
+  </div>`;
+
+  const render = () => {
+    const randomCard = () => {
+      const randomIcon = Math.floor(Math.random() * icons.length);
+      const randomNumber = Math.floor(Math.random() * numbers.length);
+      const icon = icons[randomIcon];
+      const number = numbers[randomNumber];
+
+      card.innerHTML = `<div class="card">
+    <div id="top">${icon}</div>
+    <div id="middle">${number}</div>
+    <div id="bottom">${icon}</div>
+    </div>`;
+
+      if (icon === "♦" || icon === "♥") {
+        card.innerHTML = `<div class="red">
+      <div id="top">${icon}</div>
+      <div id="middle">${number}</div>
+      <div id="bottom">${icon}</div>`;
+      } else {
+        card.innerHTML = `<div class="black">
+      <div id="top">${icon}</div>
+      <div id="middle">${number}</div>
+      <div id="bottom">${icon}</div>`;
+      }
+    };
+    randomCard();
+  };
+  render();
+  const button = document.querySelector("button");
+  button.addEventListener("click", render);
 };
